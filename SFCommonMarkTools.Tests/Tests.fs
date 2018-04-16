6afcce3ab936176cfa3c680e76bbf1bfa8bd021c
Buchanan Dunn <buchanandunn@gmail.com>
2018-04-16 07:48:22 -0400

    Added a few tests for line types

--- SFCommonMarkTools.Tests/Tests.fs
+++ SFCommonMarkTools.Tests/Tests.fs
@@ -3,6 +3,8 @@ open SFCommonMarkTools
 
 open System
 open Xunit
+open System.Threading.Tasks
+open SFCommonMarkTools.SFCommonMarkTypes
 
 [<Fact>]
 let ``My test`` () =
@@ -17,4 +19,114 @@ let ``Get Document Tree From File`` () =
     let docTree = MarkdownTools.parseMarkdownDocument markdownDoc
     Assert.True(Seq.length docTree.Items > 0)
 
+[<Fact>]
+let ``Line Type is H1`` ()=
+    let line = "# Blah"
 
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H1, result)
+    
+[<Fact>]
+let ``Line Type is H2`` ()=
+    let line = "## Blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H2, result)
+    
+[<Fact>]
+let ``Line Type is H3`` ()=
+    let line = "### Blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H3, result)
+
+[<Fact>]
+let ``Line Type is H4`` ()=
+    let line = "#### Blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H4, result)
+    
+[<Fact>]
+let ``Line Type is H5`` ()=
+    let line = "##### Blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H5, result)
+    
+[<Fact>]
+let ``Line Type is H6`` ()=
+    let line = "###### Blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.H6, result)
+
+[<Fact>]
+let ``Line Type is hr  : ---`` ()=
+    let line = "--- "
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.HR, result)
+
+[<Fact>]
+let ``Line Type is hr  : ***`` ()=
+    let line = "*** "
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.HR, result)
+
+[<Fact>]
+let ``Line Type is blank`` ()=
+    let line = ""
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.BLANK, result)
+
+[<Fact>]
+let ``Line Type is blockquote`` ()=
+    let line = ">> "
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.BLOCKQUOTE, result)
+
+[<Fact>]
+let ``Line Type is p`` ()=
+    let line = "blah blah blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.P, result)
+
+[<Fact>]
+let ``Line Type is Bullet  : - `` ()=
+    let line = "- blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.BULLET, result)
+
+[<Fact>]
+let ``Line Type is Bullet  : + `` ()=
+    let line = "+ blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.BULLET, result)
+
+[<Fact>]
+let ``Line Type is Bullet  : * `` ()=
+    let line = "* blah"
+
+    let result = MarkdownTools.getLineType line
+
+    Assert.Equal(DocumentTreeItemType.BULLET, result)
